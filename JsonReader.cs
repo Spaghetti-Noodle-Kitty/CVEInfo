67f9344a0ad80cca31cb7c4d5c99f97f0e8e1df4
Zoe <elias.m.decorvet@gmail.com>
2022-09-20 12:01:40 +0200

    Add files via upload

---
+++ JsonReader.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Automation
+{
+    internal class JsonReader
+    {
+        public static void GetCVEInfo(string JSON, bool WANTS_DESCRIPTION)
+        {
+            string BASE_METRIC_PATH = "$.result.CVE_Items.[0].impact.baseMetricV3.cvssV3."; // Set the base JSON path for most keys //
+            var res = (JObject)JsonConvert.DeserializeObject(JSON); // Parse the JSON-String as a JSON-Object //
+
+
+            // Get all CVE-Info and Save it to CVE_DATA-List //
+            List<string> CVE_DATA = new List<string>();
+
+            string PUBLISHED = res.SelectToken("$.result.CVE_Items.[0].publishedDate").ToString().Replace('T',' ').Replace("Z","");        // (YYYY-MM-TT hh:mm) //
+            string MODIFIED = res.SelectToken("$.result.CVE_Items.[0].lastModifiedDate").ToString().Replace('T', ' ').Replace("Z", "");    // (YYYY-MM-TT hh:mm) //
+
+            CVE_DATA.Add(res.SelectToken("$.result.CVE_Items.[0].cve.CVE_data_meta.ID") + " Information");                                 // Name of CVE (CVE-Number) //
+            CVE_DATA.Add("Base Score:             " + res.SelectToken(BASE_METRIC_PATH + "baseScore"));                                    // Base Score (DOUBLE) //
+            CVE_DATA.Add("Base Severity:          " + res.SelectToken(BASE_METRIC_PATH + "baseSeverity"));                                 // base severity (STRING) //
+            CVE_DATA.Add("Publishing Date:        " + PUBLISHED);
+            CVE_DATA.Add("Last Modified Date:     " + MODIFIED);
+            CVE_DATA.Add("-------------------------------------------------------");
+            CVE_DATA.Add("Vector:                 " + res.SelectToken(BASE_METRIC_PATH + "attackVector"));                                 // Attack Vector (STRING) //
+            CVE_DATA.Add("Complexity:             " + res.SelectToken(BASE_METRIC_PATH + "attackComplexity"));                             // Complexity (STRING) //
+            CVE_DATA.Add("Privileges Required:    " + res.SelectToken(BASE_METRIC_PATH + "privilegesRequired"));                           // Privileges (STRING) //
+            CVE_DATA.Add("User Interaction:       " + res.SelectToken(BASE_METRIC_PATH + "userInteraction"));                              // User Interaction (STRING) //
+            CVE_DATA.Add("Exploitability Score:   " + res.SelectToken("$.result.CVE_Items.[0].impact.baseMetricV3.exploitabilityScore"));  // Exploitability (DOUBLE) //
+            CVE_DATA.Add("-------------------------------------------------------");
+            CVE_DATA.Add("Confidentiality Impact: " + res.SelectToken(BASE_METRIC_PATH + "confidentialityImpact"));                        // Confidentiality Impact (STRING) //
+            CVE_DATA.Add("Integrity Impact:       " + res.SelectToken(BASE_METRIC_PATH + "integrityImpact"));                              // Integrity Impact (STRING) //
+            CVE_DATA.Add("Availiability Impact:   " + res.SelectToken(BASE_METRIC_PATH + "availabilityImpact"));                           // Availabilty Impact (STRING) // 
+            CVE_DATA.Add("Impact Score:           " + res.SelectToken("$.result.CVE_Items.[0].impact.baseMetricV3.impactScore"));          // Impact Score (DOUBLE) //
+            CVE_DATA.Add("-------------------------------------------------------");
+            CVE_DATA.Add("https://nvd.nist.gov/vuln/detail/" + res.SelectToken("$.result.CVE_Items.[0].cve.CVE_data_meta.ID"));            // Link to NVD entry for CVE //
+            if (WANTS_DESCRIPTION)
+            {
+                CVE_DATA.Add("-------------------------------------------------------");
+                CVE_DATA.Add(res.SelectToken("$.result.CVE_Items.[0].cve.description.description_data.[0].value").ToString());
+            }
+
+            foreach (string DATA_STRING in CVE_DATA)
+                Console.WriteLine(DATA_STRING); // Print every Line in CVE_DATA-List //
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Automation
+{
+    internal class Program
+    {
+        public static string NIST_CVE_SERV = "https://services.nvd.nist.gov/rest/json/cve/1.0/"; // The base URL to get NVD-Entries //
+        private static string API_KEY = ""; // API-Key used for heightend request number at the NVD //
+        private static bool DESCRIPTION_FLAG = false;
+
+    static void Main(string[] args)
+        {
+            try
+            {
+                if (!Regex.Match(args[0], "CVE-\\d{4}-\\d{4,7}").Success) // Test if provided CVE has a valid format //
+                    Console.WriteLine("Invalid CVE"); // mald //
+                else
+                {
+                    try
+                    {
+                        // Create a web-request using get, targeting the NVD-Json-API and pass the API-Key in URL //
+                        HttpWebRequest REQUEST_TO_NVD = (HttpWebRequest)WebRequest.Create(String.Format("https://services.nvd.nist.gov/rest/json/cve/1.0/{0}?apiKey={1}", args[0], API_KEY));
+                        REQUEST_TO_NVD.Method = "GET";
+                        // Get response from NVD and read it //
+                        WebResponse NVD_RESPONSE = REQUEST_TO_NVD.GetResponse();
+                        Stream RESPONSE_STREAM = NVD_RESPONSE.GetResponseStream();
+                        StreamReader RESPONSE_READER = new StreamReader(RESPONSE_STREAM);
+                        String JSON_RESPONSE = RESPONSE_READER.ReadToEnd();
+                        // Send read JSON to parser method //
+                        JsonReader.GetCVEInfo(JSON_RESPONSE, DESCRIPTION_FLAG);
+                    }
+                    catch
+                    { Console.WriteLine("Error Getting CVE Information"); /* mald because of network/API issue */ }
+
+                }
+            }
+            catch
+            { Console.WriteLine("You must provide a CVE-Number"); }
+            Console.WriteLine();
+            
+
+        }
+    }
+}
